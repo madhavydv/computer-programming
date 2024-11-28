f103118df05affec2b5c9c28f540cc35bb123cd4
madhavydv <madhavydv07@gmail.com>
2024-11-28 11:24:11 +0530

    Update function_q12.py

--- coding-assignment/function/function_q12.py
+++ coding-assignment/function/function_q12.py
@@ -1,1 +1,7 @@
+#Problem: Write a function to check if two strings are anagrams.
 
+def are_anagrams(s1, s2):
+    return sorted(s1) == sorted(s2)
+
+# Example usage:
+print(are_anagrams("listen", "silent"))  # Output: True
