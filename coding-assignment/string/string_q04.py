a78a748a21f51406c04921e1eecd08c9dc8d229b
madhavydv <madhavydv07@gmail.com>
2024-11-28 20:32:45 +0530

    Update string_q04.py

--- coding-assignment/string/string_q04.py
+++ coding-assignment/string/string_q04.py
@@ -1,1 +1,14 @@
+#Problem: Find the length of the longest substring without repeating characters.
+
+s = "abcabcbb"
+char_set = set()
+left = 0
+result = 0
+for right in range(len(s)):
+    while s[right] in char_set:
+        char_set.remove(s[left])
+        left += 1
+    char_set.add(s[right])
+    result = max(result, right - left + 1)
+print(result)
 
