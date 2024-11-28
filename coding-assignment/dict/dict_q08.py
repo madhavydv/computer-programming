e9ba36f4bc6a79c2d4764c3639a9be6e1e2d3091
madhavydv <madhavydv07@gmail.com>
2024-11-28 08:40:15 +0530

    Update dict_q08.py

--- coding-assignment/dict/dict_q08.py
+++ coding-assignment/dict/dict_q08.py
@@ -1,1 +1,7 @@
+#Given a list of elements, use a dictionary to count the frequency of each element.
 
+lst = ['a', 'b', 'a', 'c', 'a', 'b']
+freq_dict = {}
+for item in lst:
+    freq_dict[item] = freq_dict.get(item, 0) + 1
+print(freq_dict)
