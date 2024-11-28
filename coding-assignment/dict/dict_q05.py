8f4033396c446d08890789d8f85e11562eeec1ed
madhavydv <madhavydv07@gmail.com>
2024-11-28 08:29:18 +0530

    Update dict_q05.py

--- coding-assignment/dict/dict_q05.py
+++ coding-assignment/dict/dict_q05.py
@@ -1,1 +1,7 @@
+#Remove a list of keys from a dictionary.
 
+sample_dict = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+keys_to_remove = ['Ten', 'Thirty']
+for key in keys_to_remove:
+    del sample_dict[key]
+print(sample_dict)
