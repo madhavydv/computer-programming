dbe5fb349ef891114b99abaaa332d18b9f4ddf79
madhavydv <madhavydv07@gmail.com>
2024-11-28 08:38:56 +0530

    Update dict_q06.py

--- coding-assignment/dict/dict_q06.py
+++ coding-assignment/dict/dict_q06.py
@@ -1,1 +1,7 @@
+#Write a function that returns the key with the maximum value in a dictionary.
 
+def get_max_key(d):
+    return max(d, key=d.get)
+
+dictionary = {'a': 1, 'b': 5, 'c': 3}
+print(get_max_key(dictionary))
