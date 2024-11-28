28b445096a9783952cfdb51941df561bafa747ae
madhavydv <madhavydv07@gmail.com>
2024-11-28 11:43:42 +0530

    Update list_q15.py

--- coding-assignment/list/list_q15.py
+++ coding-assignment/list/list_q15.py
@@ -1,1 +1,5 @@
+#Problem: Given a list of lists, flatten it into a single list. Solution:
 
+lst = [[1, 2], [3, 4], [5, 6]]
+flattened = [item for sublist in lst for item in sublist]
+print(flattened)
