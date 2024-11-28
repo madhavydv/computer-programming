f8e98620c0fda1f68d8303ea1c6dfa7f47402fcf
madhavydv <madhavydv07@gmail.com>
2024-11-28 11:17:55 +0530

    Update function_q02.py

--- coding-assignment/function/function_q02.py
+++ coding-assignment/function/function_q02.py
@@ -1,1 +1,9 @@
+#Problem: Write a function to compute the factorial of a number.
 
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+# Example usage:
+print(factorial(5))  # Output: 120
