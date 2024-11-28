dab568ff55e7f621dd9f78c13ff0db8394433ad4
madhavydv <madhavydv07@gmail.com>
2024-11-28 20:36:39 +0530

    Update string_q10.py

--- coding-assignment/string/string_q10.py
+++ coding-assignment/string/string_q10.py
@@ -1,1 +1,15 @@
+#Problem: Check if a string containing just the characters '(', ')', '{', '}', '[' and ']' is valid.
 
+s = "([)]"
+stack = []
+mapping = {')': '(', '}': '{', ']': '['}
+for char in s:
+    if char in mapping:
+        top_element = stack.pop() if stack else '#'
+        if mapping[char] != top_element:
+            print("False")
+            break
+    else:
+        stack.append(char)
+else:
+    print("True" if not stack else "False")
