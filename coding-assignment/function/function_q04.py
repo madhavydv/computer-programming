a319ca8a9a7e0806e439122caf3e1bdc863ac557
madhavydv <madhavydv07@gmail.com>
2024-11-28 11:18:50 +0530

    Update function_q04.py

--- coding-assignment/function/function_q04.py
+++ coding-assignment/function/function_q04.py
@@ -1,1 +1,8 @@
+#Problem: Write a function that checks if a string is a palindrome.
 
+def is_palindrome(s):
+    return s == s[::-1]
+
+# Example usage:
+print(is_palindrome("madam"))  # Output: True
+print(is_palindrome("hello"))  # Output: False
