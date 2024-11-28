bbede6fff19686fbfcaf939064d1149ffaeee120
madhavydv <madhavydv07@gmail.com>
2024-11-28 20:33:14 +0530

    Update string_q05.py

--- coding-assignment/string/string_q05.py
+++ coding-assignment/string/string_q05.py
@@ -1,1 +1,8 @@
+#Problem: Count vowels and consonants in a string.
 
+s = "hello"
+vowels = "aeiouAEIOU"
+vowels_count = sum(1 for char in s if char in vowels)
+consonants_count = sum(1 for char in s if char.isalpha() and char not in vowels)
+print("Vowels:", vowels_count)
+print("Consonants:", consonants_count)
