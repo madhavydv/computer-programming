42cf8972deaaefc8436fdafd81e6dfbc216a4d8d
madhavydv <madhavydv07@gmail.com>
2024-11-28 11:23:13 +0530

    Update function_q10.py

--- coding-assignment/function/function_q10.py
+++ coding-assignment/function/function_q10.py
@@ -1,1 +1,7 @@
+#Problem: Write a function that returns the sum of the digits of a number.
 
+def sum_of_digits(n):
+    return sum(int(digit) for digit in str(n))
+
+# Example usage:
+print(sum_of_digits(123))  # Output: 6
