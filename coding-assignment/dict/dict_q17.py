4b97de9735fd7c35a40acaa4d2df08c374108a0d
madhavydv <madhavydv07@gmail.com>
2024-11-28 10:53:45 +0530

    Update dict_q17.py

--- coding-assignment/dict/dict_q17.py
+++ coding-assignment/dict/dict_q17.py
@@ -1,1 +1,9 @@
+#Access values in a nested dictionary.
 
+students = {
+    'student1': {'name': 'Alice', 'age': 25, 'major': 'Computer Science'},
+    'student2': {'name': 'Bob', 'age': 23, 'major': 'Mathematics'}
+}
+# Access value in nested dictionary
+student1_name = students['student1']['name']
+print(student1_name) 
