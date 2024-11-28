90a99172769b251145e728b9626db7876d12c049
madhavydv <madhavydv07@gmail.com>
2024-11-28 11:25:10 +0530

    Update function_q14.py

--- coding-assignment/function/function_q14.py
+++ coding-assignment/function/function_q14.py
@@ -1,1 +1,8 @@
+#Problem: Write a function that computes the sum of all even numbers from 1 to n.
+
+def sum_of_evens(n):
+    return sum(i for i in range(2, n + 1, 2))
+
+# Example usage:
+print(sum_of_evens(10))  # Output: 30
 
