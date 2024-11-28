b0470be74e371964adbd868c2dbbe78f9c6872fd
madhavydv <madhavydv07@gmail.com>
2024-11-28 11:46:18 +0530

    Update list_q18.py

--- coding-assignment/list/list_q18.py
+++ coding-assignment/list/list_q18.py
@@ -1,1 +1,12 @@
+#Problem: Given a list and a sum, find two numbers that add up to the given sum. Solution:
 
+lst = [1, 2, 3, 4, 5]
+target = 7
+seen = set()
+pair_found = False
+for num in lst:
+    if target - num in seen:
+        pair_found = True
+        break
+    seen.add(num)
+print(pair_found)
