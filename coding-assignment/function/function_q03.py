3196c1c23fe3e3af12e45d9a1923692671e2813d
madhavydv <madhavydv07@gmail.com>
2024-11-28 11:18:21 +0530

    Update function_q03.py

--- coding-assignment/function/function_q03.py
+++ coding-assignment/function/function_q03.py
@@ -1,1 +1,9 @@
+#Problem: Write a function that returns the nth Fibonacci number.
 
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+# Example usage:
+print(fibonacci(6))  # Output: 8
