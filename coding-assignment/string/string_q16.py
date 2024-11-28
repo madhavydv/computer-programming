032eaec4bd167c372b153feb0e6ec73f4edc42c9
madhavydv <madhavydv07@gmail.com>
2024-11-28 20:40:23 +0530

    Update string_q16.py

--- coding-assignment/string/string_q16.py
+++ coding-assignment/string/string_q16.py
@@ -1,1 +1,8 @@
+#Problem: Check if one string is a rotation of another.
 
+s1 = "waterbottle"
+s2 = "erbottlewat"
+if len(s1) == len(s2) and s2 in (s1 + s1):
+    print("True")
+else:
+    print("False")
