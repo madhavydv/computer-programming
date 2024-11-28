6031efa913825cffb76e2fa30320ec5752c7a6d5
madhavydv <madhavydv07@gmail.com>
2024-11-28 11:42:17 +0530

    Update list_q12.py

--- coding-assignment/list/list_q12.py
+++ coding-assignment/list/list_q12.py
@@ -1,1 +1,7 @@
+#Problem: Given a list of integers from 1 to N with one missing number, find the missing number. Solution:
 
+lst = [1, 2, 4, 5]
+n = len(lst) + 1
+total_sum = n * (n + 1) // 2
+missing_number = total_sum - sum(lst)
+print(missing_number)
