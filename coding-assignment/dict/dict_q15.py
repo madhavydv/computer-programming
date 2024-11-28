a1c8f8633eb632fba763fad3d3d33afad7fc6e9c
madhavydv <madhavydv07@gmail.com>
2024-11-28 08:47:40 +0530

    Update dict_q15.py

--- coding-assignment/dict/dict_q15.py
+++ coding-assignment/dict/dict_q15.py
@@ -1,1 +1,6 @@
+#Find the key with the minimum value in a dictionary.
 
+def get_min_key(d):
+    return min(d, key=d.get)
+dictionary = {'a': 3, 'b': 1, 'c': 5}
+print(get_min_key(dictionary))  # Output: 'b'
