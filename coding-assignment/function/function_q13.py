727d91bfa4fb3fd9aa29598a0347f6a36dfb7229
madhavydv <madhavydv07@gmail.com>
2024-11-28 11:24:38 +0530

    Update function_q13.py

--- coding-assignment/function/function_q13.py
+++ coding-assignment/function/function_q13.py
@@ -1,1 +1,7 @@
+#Problem: Write a function that counts how many times an element occurs in a list.
 
+def count_occurrences(lst, x):
+    return lst.count(x)
+
+# Example usage:
+print(count_occurrences([1, 2, 3, 4, 2, 2], 2))  # Output: 3
