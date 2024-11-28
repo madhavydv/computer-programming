0ce932a616a1b2b8f062b8868d6b482b84499d7b
madhavydv <madhavydv07@gmail.com>
2024-11-28 11:43:13 +0530

    Update list_q14.py

--- coding-assignment/list/list_q14.py
+++ coding-assignment/list/list_q14.py
@@ -1,1 +1,8 @@
+#Problem: Given a number N, return all prime numbers from 1 to N. Solution:
 
+n = 20
+primes = []
+for i in range(2, n+1):
+    if all(i % j != 0 for j in range(2, int(i**0.5) + 1)):
+        primes.append(i)
+print(primes)
