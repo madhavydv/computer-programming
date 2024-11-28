fe5bb1c622b97ea0a1ad4d7d33d7c388a8a5b1ea
madhavydv <madhavydv07@gmail.com>
2024-11-28 20:39:46 +0530

    Update string_q15.py

--- coding-assignment/string/string_q15.py
+++ coding-assignment/string/string_q15.py
@@ -1,1 +1,9 @@
+#Problem: Find the first non-repeating character in a string.
 
+s = "leetcode"
+for char in s:
+    if s.count(char) == 1:
+        print(char)
+        break
+else:
+    print(None)
