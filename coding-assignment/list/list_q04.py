189a7882dc9def7e43405be8cc4413b858c86526
madhavydv <madhavydv07@gmail.com>
2024-11-28 11:38:13 +0530

    Update list_q04.py

--- coding-assignment/list/list_q04.py
+++ coding-assignment/list/list_q04.py
@@ -1,1 +1,7 @@
+#Problem: Given a list of integers, find the second largest element. Solution:
 
+lst = [1, 3, 2, 5, 4]
+unique_lst = list(set(lst))
+unique_lst.sort()
+second_largest = unique_lst[-2] if len(unique_lst) > 1 else None
+print(second_largest)
