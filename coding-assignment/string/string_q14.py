bc50f713b265374a6ee66b1ce9c861b10098b6f8
madhavydv <madhavydv07@gmail.com>
2024-11-28 20:39:14 +0530

    Update string_q14.py

--- coding-assignment/string/string_q14.py
+++ coding-assignment/string/string_q14.py
@@ -1,1 +1,16 @@
+#Problem: Check if two strings are isomorphic.
 
+s1 = "egg"
+s2 = "add"
+if len(s1) != len(s2):
+    print("False")
+else:
+    mapping = {}
+    for a, b in zip(s1, s2):
+        if a not in mapping:
+            mapping[a] = b
+        elif mapping[a] != b:
+            print("False")
+            break
+    else:
+        print("True")
