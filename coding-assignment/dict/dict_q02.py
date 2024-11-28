4e29dfb2e0d964b19efe008b6e37cdf702b53191
madhavydv <madhavydv07@gmail.com>
2024-11-28 08:26:19 +0530

    Update dict_q02.py

--- coding-assignment/dict/dict_q02.py
+++ coding-assignment/dict/dict_q02.py
@@ -1,1 +1,6 @@
+#Combine two dictionaries into one.
 
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+merged_dict = {**dict1, **dict2}
+print(merged_dict)
