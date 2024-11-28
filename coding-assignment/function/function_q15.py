d91144b69dd38bd38a640b7ec603d861252b6bba
madhavydv <madhavydv07@gmail.com>
2024-11-28 11:25:33 +0530

    Update function_q15.py

--- coding-assignment/function/function_q15.py
+++ coding-assignment/function/function_q15.py
@@ -1,1 +1,7 @@
+#Problem: Write a function that converts a temperature from Celsius to Fahrenheit.
 
+def celsius_to_fahrenheit(c):
+    return (c * 9/5) + 32
+
+# Example usage:
+print(celsius_to_fahrenheit(0))  # Output: 32.0
