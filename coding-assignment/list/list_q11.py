85136075a843ca1d6c6524d6baaa5501511afd4f
madhavydv <madhavydv07@gmail.com>
2024-11-28 11:41:31 +0530

    Update list_q11.py

--- coding-assignment/list/list_q11.py
+++ coding-assignment/list/list_q11.py
@@ -1,1 +1,6 @@
+#Problem: Given a list of integers, find the maximum product of two distinct elements. Solution:
 
+lst = [1, 3, 5, 2, 4]
+lst.sort()
+max_product = lst[-1] * lst[-2]
+print(max_product)
