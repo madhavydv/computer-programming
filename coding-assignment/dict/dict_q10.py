51f91826cb921ad235af7797c79e3713305a47d5
madhavydv <madhavydv07@gmail.com>
2024-11-28 08:42:41 +0530

    Update dict_q10.py

--- coding-assignment/dict/dict_q10.py
+++ coding-assignment/dict/dict_q10.py
@@ -1,1 +1,5 @@
+#Sort a dictionary by its values in ascending order.
 
+dictionary = {'a': 3, 'b': 1, 'c': 2}
+sorted_dict = dict(sorted(dictionary.items(), key=lambda item: item[1]))
+print(sorted_dict)
