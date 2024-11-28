45ad773be1d902eacb20dad9bfddb2a386ef76d3
madhavydv <madhavydv07@gmail.com>
2024-11-28 11:39:55 +0530

    Update list_q08.py

--- coding-assignment/list/list_q08.py
+++ coding-assignment/list/list_q08.py
@@ -1,1 +1,6 @@
+#Problem: Given two lists, return a list of elements that appear in both lists. Solution:
 
+lst1 = [1, 2, 3, 4]
+lst2 = [3, 4, 5, 6]
+intersection = list(set(lst1) & set(lst2))
+print(intersection)
