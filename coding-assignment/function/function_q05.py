d3536e4479eba5af6b70b1cca79d899e2b11d128
madhavydv <madhavydv07@gmail.com>
2024-11-28 11:19:25 +0530

    Update function_q05.py

--- coding-assignment/function/function_q05.py
+++ coding-assignment/function/function_q05.py
@@ -1,1 +1,13 @@
+#Problem: Write a function that checks if a number is prime.
 
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+# Example usage:
+print(is_prime(7))  # Output: True
+print(is_prime(10)) # Output: False
