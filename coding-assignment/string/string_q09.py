56f2172922f256340c48a6fb55d8b4a5ada09f3c
madhavydv <madhavydv07@gmail.com>
2024-11-28 20:35:45 +0530

    Update string_q09.py

--- coding-assignment/string/string_q09.py
+++ coding-assignment/string/string_q09.py
@@ -1,1 +1,8 @@
+#Problem: Find the longest common prefix in a list of strings.
 
+strs = ["flower", "flow", "flight"]
+prefix = strs[0]
+for string in strs[1:]:
+    while not string.startswith(prefix):
+        prefix = prefix[:-1]
+print(prefix)
