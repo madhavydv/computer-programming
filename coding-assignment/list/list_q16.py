a9490f9128ad7431fa5ba3fccb946455d53a3345
madhavydv <madhavydv07@gmail.com>
2024-11-28 11:44:11 +0530

    Update list_q16.py

--- coding-assignment/list/list_q16.py
+++ coding-assignment/list/list_q16.py
@@ -1,1 +1,12 @@
+#Problem: Given a list of integers, find the length of the longest consecutive subsequence. Solution:
 
+lst = [100, 4, 200, 1, 3, 2]
+nums = set(lst)
+longest = 0
+for num in nums:
+    if num - 1 not in nums:
+        length = 1
+        while num + length in nums:
+            length += 1
+        longest = max(longest, length)
+print(longest)
