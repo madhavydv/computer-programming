1c15c0330300d1d2dc5965fd9929c65902f00a40
madhavydv <madhavydv07@gmail.com>
2024-11-28 11:17:20 +0530

    Update function_q01.py

--- coding-assignment/function/function_q01.py
+++ coding-assignment/function/function_q01.py
@@ -1,1 +1,7 @@
+#Write a function that computes the sum of the first n natural numbers.
 
+def sum_of_natural_numbers(n):
+    return n * (n + 1) // 2
+
+# Example usage:
+print(sum_of_natural_numbers(10))  # Output: 55
