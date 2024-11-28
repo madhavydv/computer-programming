5fd8e14c73a3194af80a1e45ad1f2c76b1e20300
madhavydv <madhavydv07@gmail.com>
2024-11-28 08:25:25 +0530

    Update dict_q01.py

--- coding-assignment/dict/dict_q01.py
+++ coding-assignment/dict/dict_q01.py
@@ -1,1 +1,6 @@
+#Given two lists, create a dictionary where one list contains the keys and the other contains the values.
 
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+res_dict = dict(zip(keys, values))
+print(res_dict)
