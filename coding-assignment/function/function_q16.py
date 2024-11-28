169aa95ecff28233ba04cc3a928f70e6e9411989
madhavydv <madhavydv07@gmail.com>
2024-11-28 11:26:07 +0530

    Update function_q16.py

--- coding-assignment/function/function_q16.py
+++ coding-assignment/function/function_q16.py
@@ -1,1 +1,7 @@
+#Problem: Write a function that returns the largest element from a list.
 
+def find_largest(lst):
+    return max(lst)
+
+# Example usage:
+print(find_largest([1, 3, 2, 5, 4]))  # Output: 5
