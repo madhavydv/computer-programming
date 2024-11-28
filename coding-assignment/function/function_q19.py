0c576a0e9ff2945d8e233e35c2f2d09569423d42
madhavydv <madhavydv07@gmail.com>
2024-11-28 11:27:18 +0530

    Update function_q19.py

--- coding-assignment/function/function_q19.py
+++ coding-assignment/function/function_q19.py
@@ -1,1 +1,10 @@
+#Problem: Write a function that multiplies all elements in a list.
 
+def multiply_elements(lst):
+    result = 1
+    for num in lst:
+        result *= num
+    return result
+
+# Example usage:
+print(multiply_elements([1, 2, 3, 4]))  # Output: 24
