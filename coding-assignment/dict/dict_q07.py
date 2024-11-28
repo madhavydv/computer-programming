afca1f6b77ff525c8900532ce08cff13c4a477c8
madhavydv <madhavydv07@gmail.com>
2024-11-28 08:39:36 +0530

    Update dict_q07.py

--- coding-assignment/dict/dict_q07.py
+++ coding-assignment/dict/dict_q07.py
@@ -1,1 +1,4 @@
+#Use dictionary comprehension to create a dictionary where the keys are numbers from 1 to 10 and the values are the squares of the keys.
 
+squares = {x: x**2 for x in range(1, 11)}
+print(squares)
