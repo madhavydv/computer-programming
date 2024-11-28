2f2b6687b6882c1fa3ee0bd3291719e0462014da
madhavydv <madhavydv07@gmail.com>
2024-11-28 08:46:22 +0530

    Update dict_q14.py

--- coding-assignment/dict/dict_q14.py
+++ coding-assignment/dict/dict_q14.py
@@ -1,1 +1,5 @@
+#Iterate through a dictionary and print both keys and values.
 
+dictionary = {'a': 1, 'b': 2, 'c': 3}
+for key, value in dictionary.items():
+    print(f"Key: {key}, Value: {value}")
