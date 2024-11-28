ab6e2d4a4f9441c44fe113e3a27d1028333fd845
madhavydv <madhavydv07@gmail.com>
2024-11-28 08:45:27 +0530

    Update dict_q13.py

--- coding-assignment/dict/dict_q13.py
+++ coding-assignment/dict/dict_q13.py
@@ -1,1 +1,6 @@
+#Find common keys between two given dictionaries.
 
+dict1 = {'a': 1, 'b': 2, 'c': 3}
+dict2 = {'b': 4, 'c': 5, 'd': 6}
+common_keys = dict1.keys() & dict2.keys()
+print(common_keys)
