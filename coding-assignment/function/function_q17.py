b6ca00c8cf317c4ee8c7daa21d9e8b8a16a4e64e
madhavydv <madhavydv07@gmail.com>
2024-11-28 11:26:29 +0530

    Update function_q17.py

--- coding-assignment/function/function_q17.py
+++ coding-assignment/function/function_q17.py
@@ -1,1 +1,7 @@
+#Problem: Write a function that returns the smallest element from a list.
 
+def find_smallest(lst):
+    return min(lst)
+
+# Example usage:
+print(find_smallest([1, 3, 2, 5, 4]))  # Output: 1
